bf0ed27c1efeeb0a91127a8a14c8db484626f1e8
David M. Weigl <musicog@users.noreply.github.com>
2019-05-24 14:29:41 +0200

    Update ApplyAnnotations.user.js
    Implement bounding box divs (WIP)

--- ApplyAnnotations.user.js
+++ ApplyAnnotations.user.js
@@ -10,6 +10,7 @@
 // ==/UserScript==
 
 function fetchAndApply() { 
+   console.log("button clicked");
    const theUri = document.getElementById('__applyannotations_userscript_url_input').value;
    // GET the content at the specified URI, as JSON-LD
    fetch(theUri, { headers: { "Accept": "application/ld+json"} }).then((response) => {
@@ -25,29 +26,57 @@ function fetchAndApply() {
               const motivations = anno["http://www.w3.org/ns/oa#motivation"] || [];
               // expect the target ID to end in a fragment identifier (e.g. #note-12345)
               const targetId = target["@id"]
-            	const fragment = targetId.substr(targetId.lastIndexOf("#"))
+            	const fragment = targetId.substr(targetId.lastIndexOf("#")+1)
               // any annotation bodies... (might be none)
               const bodies = anno["http://www.w3.org/ns/oa#hasBody"] || [];
               // the target DOM element
-            	const element = document.querySelector(fragment);
+            	const element = document.querySelector("#" + fragment);
+              // obtain the overlay div for this element
+              let overlay = document.querySelector("#overlay-"+fragment);
+              console.log("Selector overlay ", overlay);
+              if(!overlay) { 
+                // don't have one yet - so we have to build one
+                const boundRect = element.getBoundingClientRect();
+                console.log("bounding rect: ", boundRect);
+                overlay = document.createElement("div");
+                overlay.setAttribute("id", "overlay-" + fragment);
+                /*overlay.setAttribute("style", 
+                                     "position:absolute;" +
+                                     "left:" + Math.floor(boundRect.left) + "px;" +
+                										 "top:" + Math.floor(boundRect.top) + "px;" +
+                										 "width:" + Math.ceil(boundRect.right - boundRect.left) + "px;" + 
+                										 "height:" + Math.ceil(boundRect.bottom - boundRect.top) + "px;" + 
+                                     "background: rgba(255,0,0,0.8);" +
+                                     "z-index: 1;");*/
+                overlay.style.position = "absolute";
+                overlay.style.left = Math.round(boundRect.left + window.scrollX) + "px";
+                overlay.style.top = Math.round(boundRect.top + window.scrollY) + "px";
+                overlay.style.width = Math.round(boundRect.right - boundRect.left) + "px";
+                overlay.style.height = Math.round(boundRect.bottom - boundRect.top) + "px";
+                overlay.style["z-index"] = 1;
+                // insert it into the DOM
+                console.log("inserting overlay: ", overlay);
+                document.body.insertBefore(overlay, document.body.firstChild);
+              }
+                
               motivations.map((motivation) => { 
               	// now do stuff to the target element, according to the annotation's motivation
               	switch(motivation["@id"]) {
                	 case "http://www.w3.org/ns/oa#highlighting":
 										// fill in the element
                     console.log("HIGHLIGHTING: ", anno);
-            				element.style.fill = "darkorange";
+            				overlay.style.background = "rbga(255,0,0,0.8)";
                     break;
                	 case "http://www.w3.org/ns/oa#linking":
                     console.log("LINKING: ", anno);
                	   // assuming there is at least one body attached to the annotation...
                	   if(bodies.length) { 
                	     // make the target clickable, linking to the (first) body URI
-               	     element.addEventListener("click", function() { 
+               	     overlay.addEventListener("click", function() { 
                	       window.location = bodies[0]["@id"] || window.location;
                	     }, true);
                	     // and turn the cursor into a pointer as a hint that it's clickable
-               	     element.style.pointer = "cursor";
+               	     overlay.style.pointer = "cursor";
                	   }
                	   break;
                	 case "http://www.w3.org/ns/oa#describing":
@@ -56,7 +85,7 @@ function fetchAndApply() {
                	   if(bodies.length) { 
                	     // set the body (presumed to be a literal) to be the element's title text
                      console.log("setting title attribute on ", element, " to ", bodies[0]["@value"]);
-               	     element.setAttribute(title, bodies[0]["@value"] || "");
+               	     overlay.setAttribute(title, bodies[0]["@value"] || "");
                	   }
                	   break;
                	 default: 
